c171b8f38d5da00c04fbced47226ef3a709b17e5
ChitraPatel <chitrapatel126@outlook.com>
2019-03-01 14:56:07 +0530

    added code

---
+++ NLogger/App_Start/BaseController.cs
@@ -0,0 +1,36 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NLogger.App_Start
+{
+    public class BaseController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+            logger.Debug("Action Executed :" + filterContext.RouteData.Values["action"] + " of controller : " + filterContext.RouteData.Values["controller"]);
+            //base.OnActionExecuted(filterContext);
+        }
+
+        // GET: Base
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+            if (filterContext.ExceptionHandled)
+            {
+                return;
+            }
+            filterContext.Result = new ViewResult
+            {
+                ViewName = "~/Views/Shared/Error.cshtml"
+            };
+            logger.Error("Error occured in "+ filterContext.RouteData.Values["controller"] + " controller "+ filterContext.RouteData.Values["action"] + " Action", filterContext.Exception);
+
+            filterContext.ExceptionHandled = true;
+        }
+    }
+}
---
+++ NLogger/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using NLog;
+using NLog.Targets;
+using NLogger.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NLogger.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+            try
+            {
+                //logger.Debug("Error occured in");
+                LogManager.Configuration.Variables["user"] = "milind";
+                try
+                {
+                    int x = 0;
+                    int y = 5;
+                    int z = y / x;
+                   
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                    //logger.ErrorException("Error occured in Home controller Index Action", ex);
+                    //logger.ErrorException("Error occured in Home controller Index Action", ex);
+                    //logger.ErrorException("There was a serious problem communicating with on the network", webEx);
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                //NLog.LogManager.GetCurrentClassLogger().Error(ex);
+                throw ex;
+            }
+        }
+
+        public ActionResult About()
+        {
+            try
+            {
+                int x = 0;
+                int y = 5;
+                int z = y / x;
+            }
+            catch (Exception ex)
+            {
+
+
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
